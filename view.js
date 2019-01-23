3540b1b352f392c24237c1a24a38b265e9fecc09
rvion <vion.remi@gmail.com>
2019-01-23 12:02:14 +0100

    remove all dependencies

--- view.js
+++ view.js
@@ -1,22 +1,7 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const React = require("react");
-const react_dom_1 = require("react-dom");
-class App extends React.Component {
-    render() {
-        return (React.createElement("div", null,
-            React.createElement("div", null, "CHANGE-ME"),
-            React.createElement("div", null, process.cwd()),
-            React.createElement("div", null, __dirname),
-            React.createElement("div", null, __filename)));
-    }
-}
-const $root = document.getElementById("root");
-react_dom_1.render(React.createElement(App, null), $root);
-// @ts-ignore
+console.log("CHANGE-ME2");
+
 if (module.hot) {
-    // @ts-ignore
-    module.hot.accept(() => {
-        console.log("==== Reload ====");
-    });
+  module.hot.accept(() => {
+    console.log("==== Reload ====");
+  });
 }
